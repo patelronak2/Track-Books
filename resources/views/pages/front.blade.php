b9da7fbe957e860c4fce2a5d9d3e248928bb8a12
Ronak Patel <patelronak2@hotmail.com>
2019-09-06 12:52:42 -0400

    testing blade template

--- resources/views/pages/front.blade.php
+++ resources/views/pages/front.blade.php
@@ -1,7 +1,7 @@
 <!-- 
 "SOA: I Ronak Patel, 000744055 certify that this material is my original work. No other person's work has been used without due acknowledgement. 
 			  I have not made my work available to anyone else."
- -->
+
 <html>
 	<head>
 		<title>Track-Books</title>
@@ -13,8 +13,11 @@
 	</head>
 	<body>
 		<div class="card-panel teal lighten-2">This is a card panel with a teal lighten-2 class</div>
-        
-		   
-
 	</body>
 </html>
+-->
+@extends('layouts.default')
+@section('content')
+	<div class="card-panel teal lighten-2">This is a card panel with a teal lighten-2 class</div>
+
+@endsection
