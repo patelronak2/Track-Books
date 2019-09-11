7a7e0766c5516b59ae46d1db36cc431e9c96c312
Ronak Patel <patelronak2@hotmail.com>
2019-09-11 15:22:07 -0400

    bootstrap

--- resources/views/pages/front.blade.php
+++ resources/views/pages/front.blade.php
@@ -10,12 +10,14 @@
 			<h5 class="text-center">Information Goes Here!</h5>
 		</div>
 		<div class="col-md-6 col-sm-12 bg-light text-dark p-4" style="height: 80vh;">
-			<h5 class="text-center text-primary">See Who is trending in Book world!</h5>
-			<h5 class="text-center text-secondary">Join the community now!</h5>
-			<div class="text-center mt-5">
-				<a class="btn btn-primary btn-lg" href="{{ route('login') }}" role="button">{{ __('Login') }}</a>
-				
-				<a class="btn btn-outline-primary btn-lg" href="{{ route('register') }}" role="button">{{ __('Register') }}</a>
+			<div class="text-center m-5 pb-5 justify-content-center">
+				<h5 class="text-center text-primary">See Who is trending in Book world!</h5>
+				<h5 class="text-center text-secondary">Join the community now!</h5>
+				<div class="text-center mt-5">
+					<a class="btn btn-primary btn-lg" href="{{ route('login') }}" role="button">{{ __('Login') }}</a>
+					
+					<a class="btn btn-outline-primary btn-lg" href="{{ route('register') }}" role="button">{{ __('Register') }}</a>
+				</div>
 			</div>
 		</div>
 	</div>
