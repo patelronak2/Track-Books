1ae29a6acbca2ed84b4abbd183b18686b506c72d
Ronak Patel <patelronak2@hotmail.com>
2019-09-13 14:33:47 -0400

    Controller Setting

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
