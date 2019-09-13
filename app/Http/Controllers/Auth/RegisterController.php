837422b2b8861545cfa7f9f4c52fae1b58498fdc
Ronak Patel <patelronak2@hotmail.com>
2019-09-13 16:40:23 -0400

    gmail

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-		echo $data;
+		print_r($data,true);
 		
        /** return User::create([
             'name' => $data['name'],
