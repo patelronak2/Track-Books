c158ca8c807ecb22324c000aa4e77b8b628581dd
Ronak Patel <patelronak2@hotmail.com>
2019-09-13 15:39:12 -0400

    gmail

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -64,6 +64,15 @@
                                 @endif
                             </div>
                         </div>
+						<div class="container">
+						   <div class="row">
+							 <div class="col-md-12 row-block">
+							  <a href="{{ url('auth/google') }}" class="btn btn-lg btn-primary btn-block">
+							  <strong>Login With Google</strong>
+							  </a> 
+							 </div>
+						   </div>
+						</div>
                     </form>
                 </div>
             </div>
