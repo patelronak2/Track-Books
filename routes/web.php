5a928fca5ac2f9c24c79d4a984b85385401acc51
Ronak Patel <patelronak2@hotmail.com>
2019-09-15 08:44:56 -0400

    Email Verification step 1

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->middleware('verified')->name('home');
 
 Route::get('/admin', 'AdminController@admin')    
     ->middleware('is_admin')    
