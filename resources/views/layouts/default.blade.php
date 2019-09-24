e97e4c114d89d80313ce41bd816f509113bd397c
Ronak Patel <patelronak2@hotmail.com>
2019-09-24 17:41:09 -0400

    URL_CHANGED

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -40,7 +40,12 @@
                     </ul>
                   
                     <ul class="navbar-nav ml-auto">
-                       
+						<li class="nav-item">
+							<a class="nav-link" href="{{ route('login') }}">profile</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="{{ route('login') }}">Friends</a>
+						</li>
 						<li class="nav-item dropdown">
 							<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
 								{{ Auth::user()->name }} <span class="caret"></span>
