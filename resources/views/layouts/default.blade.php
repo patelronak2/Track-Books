2be145b1da8b4ac49126659161854ecd160c0749
Ronak Patel <patelronak2@hotmail.com>
2019-09-24 17:46:35 -0400

    default blade

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -40,18 +40,16 @@
                     </ul>
                   
                     <ul class="navbar-nav ml-auto">
-						<li class="nav-item">
-							<a class="nav-link" href="{{ route('login') }}">profile</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="{{ route('login') }}">Friends</a>
-						</li>
+						
 						<li class="nav-item dropdown">
 							<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
 								{{ Auth::user()->name }} <span class="caret"></span>
 							</a>
 
 							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+								
+								<a class="dropdown-item" href="{{ route('login') }}">Profile</a>
+								<a class="dropdown-item" href="{{ route('login') }}">Account Settings</a>
 								<a class="dropdown-item" href="{{ route('logout') }}"
 								   onclick="event.preventDefault();
 												 document.getElementById('logout-form').submit();">
