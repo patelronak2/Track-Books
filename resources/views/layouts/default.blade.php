7dde53226792d2ec5f35ffde08afc35a940fd0e3
Ronak Patel <patelronak2@hotmail.com>
2019-09-28 14:01:44 -0400

    Routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -48,8 +48,8 @@
 
 							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 								
-								<a class="dropdown-item" href="{{ route('profile') }}">Profile</a>
-								<a class="dropdown-item" href="{{ route('setting') }}">Account Settings</a>
+								<a class="dropdown-item" href="/public/profile">Profile</a>
+								<a class="dropdown-item" href="/public/setting">Account Settings</a>
 								<a class="dropdown-item" href="{{ route('logout') }}"
 								   onclick="event.preventDefault();
 												 document.getElementById('logout-form').submit();">
