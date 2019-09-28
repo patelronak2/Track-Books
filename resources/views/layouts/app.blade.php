9afa77095737216705830ed2d1b74cc7ded7423e
Ronak Patel <patelronak2@hotmail.com>
2019-09-28 15:25:29 -0400

    navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,9 +32,15 @@
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-				<li class="nav-item">
-                                <a class="nav-link" href="#" id="notification"><span class="fa fa-bell bg-secondry"></span></a>
-                            </li>
+				
+				
+				
+				<ul class="nav navbar-nav navbar-right">
+					<li class="nav-item">
+						<a class="nav-link" href="#" id="notification"><span class="fa fa-bell bg-secondry"></span></a>
+					</li>
+                </ul>
+				
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                
                     <ul class="navbar-nav mr-auto">
