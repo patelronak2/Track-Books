a4fb50f4263bdba4e822a3bd4044c92087721145
Ronak Patel <patelronak2@hotmail.com>
2019-10-12 18:21:37 -0400

    Database updated

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,29 +5,35 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
 			<h1>Admin Dashboard</h1>
-            <div class="card">
-                <div class="card-header">Manage users</div>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card">
+						<div class="card-header">Manage users</div>
 
-                <div class="card-body">
-                    Add User, Delete User, Ban User
-                </div>
-            </div>
-			
-			<div class="card">
-                <div class="card-header">Manage Books</div>
+						<div class="card-body">
+							Add User, Delete User, Ban User
+						</div>
+					</div>
+					</div>
+				<div class="col-md-4">
+					<div class="card">
+						<div class="card-header">Manage Books</div>
 
-                <div class="card-body">
-                    Add Books, Delete Books
-                </div>
-            </div>
-			
-			<div class="card">
-                <div class="card-header">Manage Authors</div>
+						<div class="card-body">
+							Add Books, Delete Books
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card">
+						<div class="card-header">Manage Authors</div>
 
-                <div class="card-body">
-                    Add User, Delete User, Ban User
-                </div>
-            </div>
+						<div class="card-body">
+							Add User, Delete User, Ban User
+						</div>
+					</div>	
+				</div>
+			</div>
         </div>
     </div>
 </div>
