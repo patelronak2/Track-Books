8943f68a8330f95f333970f0a3e148c0464716f5
Ronak Patel <patelronak2@hotmail.com>
2019-10-12 18:30:14 -0400

    Database updated

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,33 +5,31 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
 			<h1>Admin Dashboard</h1>
-			<div class="row">
-				<div class="col-md-4">
-					<div class="card">
-						<div class="card-header">Manage users</div>
-
-						<div class="card-body">
-							Add User, Delete User, Ban User
-						</div>
-					</div>
-					</div>
-				<div class="col-md-4">
-					<div class="card">
-						<div class="card-header">Manage Books</div>
-
-						<div class="card-body">
-							Add Books, Delete Books
-						</div>
+			<div class="mt-2">
+				<div class="card">
+					<div class="card-header">Manage users</div>
+					<div class="card-body">
+						Add User account, Delete User account, Ban User account
+						<a href="#" class="stretched-link"></a>
 					</div>
 				</div>
-				<div class="col-md-4">
-					<div class="card">
-						<div class="card-header">Manage Authors</div>
-
-						<div class="card-body">
-							Add User, Delete User, Ban User
-						</div>
-					</div>	
+			</div>
+			<div class="mt-2">
+				<div class="card">
+					<div class="card-header">Manage Books</div>
+					<div class="card-body">
+						Add Books, Delete Books
+						<a href="#" class="stretched-link"></a>
+					</div>
+				</div>
+			</div>
+			<div class="mt-2">
+				<div class="card">
+					<div class="card-header">Manage Authors</div>
+					<div class="card-body">
+						Add Author, Delete author
+						<a href="#" class="stretched-link"></a>
+					</div>
 				</div>
 			</div>
         </div>
