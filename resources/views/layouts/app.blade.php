901598a4b431c88c8cfd5925e27aaf55edf4c90c
Ronak Patel <patelronak2@hotmail.com>
2019-10-31 18:51:54 -0400

    remember me

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,15 +39,15 @@
                     </ul>
                     <ul class="navbar-nav ml-auto">
                        
-                        @guest
+                        <!--@guest -->
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            @if (Route::has('register'))
+                            <!-- @if (Route::has('register')) -->
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
-                            @endif
+                            <!-- @endif 
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -66,7 +66,7 @@
                                     </form>
                                 </div>
                             </li>
-                        @endguest
+                        @endguest -->
                     </ul>
                 </div>
             </div>
