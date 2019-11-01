f5ab4e56cb405efd2b4685ac40ea9b223b535a15
Ronak Patel <patelronak2@hotmail.com>
2019-11-01 14:15:27 -0400

    Shadow

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -35,12 +35,13 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                
                     <ul class="navbar-nav mr-auto">
-						<a href="#" class="nav-link"><span class="badge badge-warning ml-2">4</span><i class="fas fa-bell" style="font-size: 20px"></i></a>
+						
 						<li class="nav-item">
 							<form class="form-inline my-2 my-md-0">
 								<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
 							</form>
 						</li>
+						<a href="#" class="nav-link"><span class="badge badge-danger ml-2">4</span><i class="fas fa-bell" style="font-size: 20px"></i></a>
                     </ul>
 					
                     <ul class="navbar-nav ml-auto">
