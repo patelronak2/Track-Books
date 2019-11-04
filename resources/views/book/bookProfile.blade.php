156c2286efb008b47078be65b8f1abf6533a02d7
Ronak Patel <patelronak2@hotmail.com>
2019-11-03 21:50:42 -0500

    Searchby

--- resources/views/book/bookProfile.blade.php
+++ resources/views/book/bookProfile.blade.php
@@ -4,14 +4,14 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10">
-			<h2 class="text-center">{{ $book->title }}</h2>
-			<div class="card mb-3" style="max-width: 540px;">
+			<div class="card mb-3">
 			  <div class="row no-gutters">
 				<div class="col-md-4">
 				  <img src="{{ $book->img_link }}" class="card-img" alt="...">
 				</div>
 				<div class="col-md-8">
 				  <div class="card-body">
+					<h3 class="text-center">{{ $book->title }}</h3>
 					<h5 class="card-title">Author: {{ $book->author }}</h5>
 					<p class="card-text">{{ $book->description }}</p>
 					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
