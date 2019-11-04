de8e2f6375f1818e186a81b9632a72c8b0e531af
Ronak Patel <patelronak2@hotmail.com>
2019-11-03 21:37:53 -0500

    Searchby

--- resources/views/book/bookProfile.blade.php
+++ resources/views/book/bookProfile.blade.php
@@ -4,12 +4,14 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10">
-			<h2>{{ $book->title }}</h2>
-            <div class="col-md-5">
-                <img src="{{ $book->img_link }}" class="img-fluid" alt="Responsive image">
-            </div>
-			<div class="col-md-7">
-				Book Details will go here
+			<h2 class="text-center">{{ $book->title }}</h2>
+			<div class="card bg-dark text-white">
+			  <img src="{{ $book->img_link }}" class="card-img" alt="...">
+			  <div class="card-img-overlay">
+				<h5 class="card-title">Card title</h5>
+				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				<p class="card-text">Last updated 3 mins ago</p>
+			  </div>
 			</div>
         </div>
     </div>
