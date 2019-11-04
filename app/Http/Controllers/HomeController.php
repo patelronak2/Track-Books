dd0dfb67e219d4a4295d58e0bb7797773bb6c5a3
Ronak Patel <patelronak2@hotmail.com>
2019-11-04 01:33:54 -0500

    Searchby

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -67,7 +67,7 @@ class HomeController extends Controller
 		$book_id = $request->input('id');
 		$review = $request->input('review');
 		
-		$review = new $review;
+		$review = new Review;
 		$review->user_id = $user_id;
 		$review->book_id = $book_id;
 		$review->review = $review;
