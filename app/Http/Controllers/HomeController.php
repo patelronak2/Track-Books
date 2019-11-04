a1478ddd78cb5cca2cf177fc4863394820d44f8d
Ronak Patel <patelronak2@hotmail.com>
2019-11-04 01:11:58 -0500

    Searchby

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -59,22 +59,22 @@ class HomeController extends Controller
 	
 	public function addReview(Request $request)
 	{
-		$validatedData = $request->validate([
-            'review' => ['required'],			
-        ]);
+		// $validatedData = $request->validate([
+            // 'review' => ['required'],			
+        // ]);
 		
 		$user_id = Auth::user()->id;
 		$book_id = $request->input('id');
 		$review = $request->input('review');
 		
-		$review = new $review;
-		$review->user_id = $user_id;
-		$review->book_id = $book_id;
-		$review->review = $review;
-		$review->save();
+		// $review = new $review;
+		// $review->user_id = $user_id;
+		// $review->book_id = $book_id;
+		// $review->review = $review;
+		// $review->save();
 		
-		$reviews = Review::all();
+		// $reviews = Review::all();
 		
-		return $reviews;
+		return $user_id;
 	}
 }
