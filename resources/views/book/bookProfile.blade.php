bc3befed4c91de9c02df905c4b1b8bb8908bfc1d
Ronak Patel <patelronak2@hotmail.com>
2019-11-03 22:31:49 -0500

    Searchby

--- resources/views/book/bookProfile.blade.php
+++ resources/views/book/bookProfile.blade.php
@@ -3,19 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-10">
-			<div class="mb-3">
-			<h3 class="text-center">{{ $book->title }}</h3>	
+        <div class="col-md-12">
+			<div class="mb-3 shadow-sm">
+			  <h3 class="text-center mb-3">{{ $book->title }}</h3>	
 			  <div class="row no-gutters">
-				<div class="col-md-4">
-				  <img src="{{ $book->img_link }}" class="img-thumbnail" alt="...">
+				<div class="col-md-4 px-2">
+				  <img src="{{ $book->img_link }}" class="img-thumbnail" alt="Image not Available" width="100%">
 				</div>
-				<div class="col-md-8">
-				  <div class="">
+				<div class="col-md-8 p-2">
 					<h5 class="card-title">Author: {{ $book->author }}</h5>
 					<p class="card-text">{{ $book->description }}</p>
 					<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-				  </div>
 				</div>
 			  </div>
 			</div>
