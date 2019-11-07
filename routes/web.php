480fc533ff490bb2903f710ea51b5b1ee3067bbd
Ronak Patel <patelronak2@hotmail.com>
2019-11-07 00:56:38 -0500

    Notifications

--- routes/web.php
+++ routes/web.php
@@ -48,8 +48,7 @@ Route::get('/test',function(){
 	
 	//$user->notify( new App\Notifications\ShelfUpdated($user, $book, $shelf));
 	
-	foreach(Auth::user()->unreadNotifications as $notification){
-		//$notification->markAsRead();
-		dd($notification);
+	foreach(Auth::user()->notifications as $notification){
+		$notification->markAsRead();
 	}
 });
