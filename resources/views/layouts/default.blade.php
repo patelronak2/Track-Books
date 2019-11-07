6051515e3510d4254dc62fdd2536ce5773a407ac
Ronak Patel <patelronak2@hotmail.com>
2019-11-06 22:38:34 -0500

    notifications test

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -252,27 +252,10 @@
 
 							<div class="dropdown-menu dropdown-menu-right animate slideIn" aria-labelledby="notifications">
 								<a class="dropdown-item" href="#">No Notifications Yet</a>
+								<span id="newNotifications"></span>
 							</div>
 						</li>
-						<li class="dropdown dropdown-notifications">
-						  <a href="#notifications-panel" class="dropdown-toggle" data-toggle="dropdown">
-							<i data-count="0" class="glyphicon glyphicon-bell notification-icon"></i>
-						  </a>
-
-						  <div class="dropdown-container">
-							<div class="dropdown-toolbar">
-							  <div class="dropdown-toolbar-actions">
-								<a href="#">Mark all as read</a>
-							  </div>
-							  <h3 class="dropdown-toolbar-title">Notifications (<span class="notif-count">0</span>)</h3>
-							</div>
-							<ul class="dropdown-menu">
-							</ul>
-							<div class="dropdown-footer text-center">
-							  <a href="#">View All</a>
-							</div>
-						  </div>
-						</li>
+						
                     </ul>
 					
                     <ul class="navbar-nav ml-auto">
@@ -348,16 +331,6 @@
 		</div>
     </div>
 	<script type="text/javascript">
-      var notificationsWrapper   = $('.dropdown-notifications');
-      var notificationsToggle    = notificationsWrapper.find('a[data-toggle]');
-      var notificationsCountElem = notificationsToggle.find('i[data-count]');
-      var notificationsCount     = parseInt(notificationsCountElem.data('count'));
-      var notifications          = notificationsWrapper.find('ul.dropdown-menu');
-
-      if (notificationsCount <= 0) {
-        notificationsWrapper.hide();
-      }
-
       // Enable pusher logging - don't include this in production
       // Pusher.logToConsole = true;
 
@@ -370,19 +343,41 @@
 
       // Bind a function to a Event (the full Laravel class)
       channel.bind('App\\Events\\StatusLiked', function(data) {
-        var existingNotifications = notifications.html();
-        var avatar = Math.floor(Math.random() * (71 - 20 + 1)) + 20;
-        var newNotificationHtml = '<li class="notification active"><div class="media"><div class="media-left"><div class="media-object">';
-		newNotificationHtml += '<img src="https://api.adorable.io/avatars/71/`+avatar+`.png" class="img-circle" alt="50x50" style="width: 50px; height: 50px;"></div></div>';
-		newNotificationHtml += '<div class="media-body"><strong class="notification-title">'+data.message+'</strong>';
-		newNotificationHtml += '</div></div></li>';
-        notifications.html(newNotificationHtml + existingNotifications);
-
-        notificationsCount += 1;
-        notificationsCountElem.attr('data-count', notificationsCount);
-        notificationsWrapper.find('.notif-count').text(notificationsCount);
-        notificationsWrapper.show();
+        //Update dropdown from here
+		$("#newNotifications").html(data.message);
       });
     </script>  
 </body>
 </html>
+<!--
+var existingNotifications = notifications.html();
+var avatar = Math.floor(Math.random() * (71 - 20 + 1)) + 20;
+var newNotificationHtml = '<li class="notification active"><div class="media"><div class="media-left"><div class="media-object">';
+newNotificationHtml += '<img src="https://api.adorable.io/avatars/71/`+avatar+`.png" class="img-circle" alt="50x50" style="width: 50px; height: 50px;"></div></div>';
+newNotificationHtml += '<div class="media-body"><strong class="notification-title">'+data.message+'</strong>';
+newNotificationHtml += '</div></div></li>';
+notifications.html(newNotificationHtml + existingNotifications);
+
+notificationsCount += 1;
+notificationsCountElem.attr('data-count', notificationsCount);
+notificationsWrapper.find('.notif-count').text(notificationsCount);
+notificationsWrapper.show();
+<li class="dropdown dropdown-notifications">
+  <a href="#notifications-panel" class="dropdown-toggle" data-toggle="dropdown">
+	<i data-count="0" class="glyphicon glyphicon-bell notification-icon"></i>
+  </a>
+
+  <div class="dropdown-container">
+	<div class="dropdown-toolbar">
+	  <div class="dropdown-toolbar-actions">
+		<a href="#">Mark all as read</a>
+	  </div>
+	  <h3 class="dropdown-toolbar-title">Notifications (<span class="notif-count">0</span>)</h3>
+	</div>
+	<ul class="dropdown-menu">
+	</ul>
+	<div class="dropdown-footer text-center">
+	  <a href="#">View All</a>
+	</div>
+  </div>
+</li>-->
