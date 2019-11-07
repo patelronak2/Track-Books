86d35d972254713cf5b708ddd1ac3961a0ec8738
Ronak Patel <patelronak2@hotmail.com>
2019-11-07 10:56:29 -0500

    Notifications

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -148,7 +148,7 @@ class HomeController extends Controller
 			$shelf->save();
 			//fire notification to show that book has been added to shelf
 		}
-		$Auth::user()->notify( new App\Notifications\ShelfUpdated(Auth::user(), Book::findorfail($book_id), $bookShelf));
-		return $Auth::user()->notifications;
+		Auth::user()->notify( new App\Notifications\ShelfUpdated(Auth::user(), Book::findorfail($book_id), $bookShelf));
+		return Auth::user()->notifications;
 	}
 }
