9d5bb2d22fc2e543997e89733d548e66d3a6deb8
Ronak Patel <patelronak2@hotmail.com>
2019-11-07 12:13:12 -0500

    Notifications

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -336,10 +336,10 @@
 			//alert(response[0].data.shelf);
 			var temp = "";
 			for(notification of response){
-				//if(notification.type == "App\Notifications\ShelfUpdated"){
+				if(notification.type == "App\Notifications\ShelfUpdated"){
 					temp += 'Book: ' + notification.data['book_id'] + ' added to "' + notification.data['shelf'] + '" Shelf.\n';
 				
-				//}
+				}
 			}
 			alert(temp);
 		}
