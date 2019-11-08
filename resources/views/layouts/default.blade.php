a7da4c561242f2f786758e2c9646147740b5cdb6
Ronak Patel <patelronak2@hotmail.com>
2019-11-07 19:02:40 -0500

    Notifications

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -251,16 +251,13 @@
 							</a>
 
 							<div class="dropdown-menu dropdown-menu-right animate slideIn" aria-labelledby="notifications">
-								<div class="list-group">
-								  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
-									<div class="d-flex w-100 justify-content-between">
-									  <h5 class="mb-1">List group item heading</h5>
-									  <small>3 days ago</small>
-									</div>
-									<p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-									<small>Donec id elit non mi porta.</small>
-								  </a>
-								</div>
+								<ul class="list-group list-group-flush">
+								  <li class="list-group-item">Cras justo odio</li>
+								  <li class="list-group-item">Dapibus ac facilisis in</li>
+								  <li class="list-group-item">Morbi leo risus</li>
+								  <li class="list-group-item">Porta ac consectetur ac</li>
+								  <li class="list-group-item">Vestibulum at eros</li>
+								</ul>
 							</div>
 						</li>
 						
