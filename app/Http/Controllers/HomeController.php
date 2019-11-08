6b0bb64061c11b585b14aa6d42227a1473839f37
Ronak Patel <patelronak2@hotmail.com>
2019-11-07 23:03:48 -0500

    Notifications

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -169,6 +169,6 @@ class HomeController extends Controller
 			$data[][0] = $notification;
 			$data[][1] = $notification->created_at->diffForHumans();
 		}
-		return json_encode($data);
+		return $data;
 	}
 }
