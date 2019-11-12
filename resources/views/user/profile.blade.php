b79332f403ca01ff684250a051267ab548e5f3ea
Ronak Patel <patelronak2@hotmail.com>
2019-11-12 18:25:25 -0500

    fix notifications

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -8,7 +8,13 @@
 	<div class="my-3">
 		<div class="row no-gutters shadow-sm bg-light p-3">
 			<div class="col-md-6">
-				
+				<h5>Email: {{ $profile->email }}</h5>
+				@if($profile->birthday)
+					<h5>Birth date: {{ $profile->birthday }}</h5>
+				@else
+					<h5>Birth date: Not entered</h5>
+				<h5>Gender: </h5>
+				<h5>Account Setting</h5>
 			</div>
 			<div class="col-md-3"></div>
 			<div class="col-md-3"></div>
