0c5820e5917573f1d7044ee5b3e7de21ec2865de
Ronak Patel <patelronak2@hotmail.com>
2019-11-12 19:38:00 -0500

    fix notifications

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -6,8 +6,8 @@
 		<h2>{{ $profile->name }}'s Profile</h2>
 	</div>
 	<div class="my-3">
-		<div class="row no-gutters shadow-sm bg-light p-3">
-			<div class="col-md-6">
+		<div class="row no-gutters p-3">
+			<div class="col-md-3 m-1 shadow-sm bg-light">
 				<h4>Personal Information</h4>
 				<p><span class="font-weight-bold">Email:</span> {{ $profile->email }}</p>
 				@if($profile->birthday)
@@ -20,11 +20,13 @@
 				@else	
 					<p><span class="font-weight-bold">Gender:</span> Information not entered</p>
 				@endif
+				
+				<a href="/public/editProfile" class="btn btn-primary m-3">Edit Profile</a>
 			</div>
-			<div class="col-md-3">
+			<div class="col-md-3 m-1 shadow-sm bg-light">
 				<h4>Book Shelf</h4>
 			</div>
-			<div class="col-md-3">
+			<div class="col-md-3 m-1 shadow-sm bg-light">
 				<h4>Friend List</h4>
 			</div>
 		</div>
