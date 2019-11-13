6b7258196a2af9a7af109c33b294c8c490e2384d
Ronak Patel <patelronak2@hotmail.com>
2019-11-12 19:47:18 -0500

    fix notifications

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -6,8 +6,8 @@
 		<h2>{{ $profile->name }}'s Profile</h2>
 	</div>
 	<div class="my-3">
-		<div class="row no-gutters p-3">
-			<div class="col-md-3 m-1 shadow-sm bg-light">
+		<div class="row no-gutters shadow-sm bg-light p-3">
+			<div class="col-md-4">
 				<h4>Personal Information</h4>
 				<p><span class="font-weight-bold">Email:</span> {{ $profile->email }}</p>
 				@if($profile->birthday)
@@ -23,11 +23,13 @@
 				
 				<a href="/public/editProfile" class="btn btn-primary m-3">Edit Profile</a>
 			</div>
-			<div class="col-md-3 m-1 shadow-sm bg-light">
-				<h4>Book Shelf</h4>
-			</div>
-			<div class="col-md-3 m-1 shadow-sm bg-light">
-				<h4>Friend List</h4>
+			<div class="col-md-4">
+				<h4>Account Preferences</h4>
+				@if($profile->isPrivate)
+					<p><span class="font-weight-bold">Account Visibility:</span> Private</p>
+				@else	
+					<p><span class="font-weight-bold">Account Visibility:</span> Public</p>
+				@endif
 			</div>
 		</div>
 	</div>
