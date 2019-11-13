8746b0a95bacf3afe14e7c0abd9dd39409674c00
Ronak Patel <patelronak2@hotmail.com>
2019-11-13 17:58:36 -0500

    accountSetting

--- resources/views/user/setting.blade.php
+++ resources/views/user/setting.blade.php
@@ -6,13 +6,18 @@
 	<div class="my-3 bg-light shadow-sm p-3">
 		<h5>Edit Profile</h5>
 		<form method="POST" action="/public/editProfile">
-			<div class="form-group row">
-				<label for="name" class="col-md-3 col-form-label">Name</label>
-
-				<div class="col-md-4">
-					<input id="name" type="text" class="form-control" name="name" value="Ronak Patel">
+			 <div class="form-group row">
+				<label for="name" class="col-sm-2 col-form-label">Name</label>
+				<div class="col-sm-10">
+				  <input type="text" class="form-control" id="name" value="John Doe">
 				</div>
-			</div>
+			  </div>
+			  <div class="form-group row">
+				<label for="birthdate" class="col-sm-2 col-form-label">Birth Date</label>
+				<div class="col-sm-10">
+				  <input type="text" class="form-control" id="birthdate" placeholder="18th November, 1998">
+				</div>
+			  </div>
 			<div class="form-group row mb-0">
 				<div class="col-md-6 offset-md-4">
 					<button type="submit" class="btn btn-primary">
