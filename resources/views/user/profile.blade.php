cc2a691f8dc8052b5e79f43847de55bfaea570dd
Ronak Patel <patelronak2@hotmail.com>
2019-11-12 19:29:10 -0500

    fix notifications

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -9,23 +9,24 @@
 		<div class="row no-gutters shadow-sm bg-light p-3">
 			<div class="col-md-6">
 				<h4>Personal Information</h4>
-				<div>
-					<p><span class="font-weight-bold">Email:</span> {{ $profile->email }}</p>
-					@if($profile->birthday)
-						<p><span class="font-weight-bold">Birth date:</span> {{ $profile->birthday }}</p>
-					@else
-						<p><span class="font-weight-bold">Birth date:</span> Information Not Entered</p>
-					@endif
-					@if($profile->gender)
-						<p><span class="font-weight-bold">Gender:</span> </p>
-					@else	
-						<p><span class="font-weight-bold">Gender:</span> Information Not Entered</p>
-					@endif
-					
-				</div>
+				<p><span class="font-weight-bold">Email:</span> {{ $profile->email }}</p>
+				@if($profile->birthday)
+					<p><span class="font-weight-bold">Birth date:</span> {{ $profile->birthday }}</p>
+				@else
+					<p><span class="font-weight-bold">Birth date:</span> Information not entered</p>
+				@endif
+				@if($profile->gender)
+					<p><span class="font-weight-bold">Gender:</span> </p>
+				@else	
+					<p><span class="font-weight-bold">Gender:</span> Information not entered</p>
+				@endif
 			</div>
-			<div class="col-md-3"></div>
-			<div class="col-md-3"></div>
+			<div class="col-md-3">
+				<h4>Book Shelf</h4>
+			</div>
+			<div class="col-md-3">
+				<h4>Friend List</h4>
+			</div>
 		</div>
 	</div>
 </div>
