b6a22f77f773c0345611eea9b4ac7a52bdce2466
Ronak Patel <patelronak2@hotmail.com>
2019-11-13 17:02:29 -0500

    accountSetting

--- resources/views/user/setting.blade.php
+++ resources/views/user/setting.blade.php
@@ -4,13 +4,13 @@
 <div class="container">
     <h2>Account Setting</h2>
 	<div class="my-3 bg-light shadow-sm p-3">
-		<h3>Edit Profile</h3>
+		<h5>Edit Profile</h5>
 		<form method="POST" action="/public/editProfile">
 			<div class="form-group row">
-				<label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
+				<label for="name" class="col-md-3 col-form-label">Name</label>
 
-				<div class="col-md-6">
-					<input id="name" type="text" class="form-control" name="name" value="" autofocus>
+				<div class="col-md-4">
+					<input id="name" type="text" class="form-control" name="name" value="Ronak Patel">
 				</div>
 			</div>
 			<div class="form-group row mb-0">
