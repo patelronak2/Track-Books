a9853c028aaa406ffa70e118326702a1b9ad2df1
Ronak Patel <patelronak2@hotmail.com>
2019-11-14 10:02:35 -0500

    setting

--- app/Http/Controllers/User.php
+++ app/Http/Controllers/User.php
@@ -61,7 +61,7 @@ class User extends Controller
 	public function editProfile(Request $request){
 		$user = Auth::user();
 		$profiles = Profile::where('user_id', $user->id)->get();
-		echo "$profiles[0]->name";
+		echo "$profiles[0]['name']";
 		
 	}
 }
