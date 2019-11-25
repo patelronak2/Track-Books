abcb625d82e929c6b4d3046c157ed3d98bfc945d
Ronak Patel <patelronak2@hotmail.com>
2019-11-24 23:26:25 -0500

    Friend Notificaton

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,32 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="container-fluid">
-	<section class="row new-post mb-2" style="border-bottom: 1px solid #756446">
-		<div class="col-md-6 offset-md-3">
-			<header>
-				<h2>What's on your mind?</h2>
-			</header>
-			<form action="" >
-				@csrf
-				<div class="form-group">
-					<textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Write some thing here..."></textarea>
-				</div>
-				<button class="btn btn-light button" type="submit">Create Post</button>
-			</form>
-		</div>
-	</section>
-	<section class="row posts mt-3 pt-3">
-		<div class="col-md-6 offset-md-3">
-			<header class="my-2">
-				<h3>What other people are doing</h3>
-			</header>
-			<article class="post pl-1">
-				<p>Post Content Goes here</p>
-				<div class="info">
-					<h5>Posted by: Ronak Patel</h5>
-				</div>
-			</article>
-		</div>
-	</section>
 
-</div>
+<section class="row new-post p-3 border-bottom-2">
+	<div class="col-md-6 offset-md-3">
+		<header>
+			<h2>What's on your mind?</h2>
+		</header>
+		<form action="" >
+			@csrf
+			<div class="form-group">
+				<textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Write some thing here..."></textarea>
+			</div>
+			<button class="btn btn-light button" type="submit">Create Post</button>
+		</form>
+	</div>
+</section>
+<section class="row posts mt-3 p-3">
+	<div class="col-md-6 offset-md-3">
+		<header class="my-2">
+			<h3>What other people are doing</h3>
+		</header>
+		<article class="post pl-1">
+			<p>Post Content Goes here</p>
+			<div class="info">
+				<h5>Posted by: Ronak Patel</h5>
+			</div>
+		</article>
+	</div>
+</section>
 @endsection
