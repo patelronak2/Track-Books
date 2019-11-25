856ba94e40c5a94ffd8b86176649677f63d10b25
Ronak Patel <patelronak2@hotmail.com>
2019-11-24 22:52:30 -0500

    Friend Notificaton

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,20 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<div class="container-fluid">
+	<section class="row new-post">
+		<div class="col-md-6 col-md-offset-3">
+			<header>
+				<h2>What's on your mind?</h2>
+			</header>
+			<form>
+				<div class="form-group">
+					<textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Write some thing here..." />
+				</div>
+				<button class="btn btn-light button" type="submit">Create Post</button>
+			</form>
+		</div>
+	</section>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
 @endsection
