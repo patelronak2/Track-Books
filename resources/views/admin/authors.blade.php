722bb0a28b15354844795b14d0262970f1d5a0c0
Ronak Patel <patelronak2@hotmail.com>
2019-11-25 15:19:30 -0500

    posts

--- resources/views/admin/authors.blade.php
+++ resources/views/admin/authors.blade.php
@@ -1,21 +1,18 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <h4 class="card-header">Manage Authors</h4>
-
-                <div class="card-body">
-					<div class="text-center">
-						<a href="/public/addEntries" class="btn btn-primary m-1 p-2">Add Authors</a>
-						<a href="/public/admin" class="btn btn-dark m-1 p-2">Back to Dashboard</a>
-					</div>
-                    <div class="mt-5 text-center"><h3>No Author in the database yet</h3></div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="container-fluid">
+	<h2>Manage Authors</h2>
+	<div class="card">
+		<h4 class="card-header">Manage Authors</h4>
+		<div class="container-fluid">
+			<a href="/public/addEntries" class="badge badge-light m-1 p-2">Add Author</a>
+			<a href="/public/admin" class="badge badge-secondary m-1 p-2">Back to Dashboard</a>
+		</div>
+		<div class="bg-light shadow-sm p-5">
+			<h5 class="text-center">No Authors in the database.</h5>
+		</div>
+	</div>
+        
 </div>
 @endsection
