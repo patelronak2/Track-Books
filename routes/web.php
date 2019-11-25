422c092e6531aee367daee1e5569d268f9821264
Ronak Patel <patelronak2@hotmail.com>
2019-11-24 19:34:30 -0500

    Friend Notificaton

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ Route::get('/', function () {
 });
 
 Auth::routes(['verify' => true]);
-Route::get('/profile','UserController@index');
-Route::get('/setting','UserController@setting')->middleware('verified')->name('home');
+Route::get('/profile','UserController@index')->middleware('verified');
+Route::get('/setting','UserController@setting')->middleware('verified');
 Route::get('/home', 'HomeController@index')->middleware('verified')->name('home');
 Route::get('/admin', 'AdminController@admin')->middleware('is_admin')->name('admin');
 Route::get('/manageUsers','AdminController@manageUsers')->middleware('is_admin')->name('admin');
@@ -33,31 +33,31 @@ Route::get('/deleteBook/{id}','AdminController@deleteBook')->middleware('is_admi
 Route::get('/addMultipleEntries','AdminController@addMultipleEntries')->middleware('is_admin')->name('admin');
 Route::post('/insertMultipleBooks', 'AdminController@insertMultipleBooks')->middleware('is_admin')->name('admin');
 Route::post('/ajaxBookInsert', 'AdminController@ajaxBookInsert')->middleware('is_admin')->name('admin');
-Route::post('/searchInsert', 'AdminController@searchInsert');
-Route::get('/showBook/{id}','HomeController@showBook');
-Route::post('/addReview', 'HomeController@addReview');
-Route::post('/deleteReview', 'HomeController@deleteReview');
+Route::post('/searchInsert', 'AdminController@searchInsert')->middleware('verified');;
+Route::get('/showBook/{id}','HomeController@showBook')->middleware('verified');;
+Route::post('/addReview', 'HomeController@addReview')->middleware('verified');;
+Route::post('/deleteReview', 'HomeController@deleteReview')->middleware('verified');;
 Route::get('/manageReviews','AdminController@manageReviews')->middleware('is_admin')->name('admin');
 Route::get('/deleteReview/{id}', 'AdminController@deleteReview' )->middleware('is_admin')->name('admin');
-Route::post('/addToShelf','HomeController@addToShelf');
-Route::get('/count', 'HomeController@getNotificationCount');
-Route::get('/getNotification','HomeController@getNotifications');
-Route::post('/rateBook', 'HomeController@rateBook');
-Route::get('/getProfileDetails','UserController@getProfileDetails');
-Route::post('/editProfile', 'UserController@editProfile');
-Route::get('/deleteShelfBook/{id}', 'UserController@deleteShelfBook');
-Route::get('/getUserList','HomeController@getUserList');
-Route::get('/showProfile/{id}', 'HomeController@showProfile');
+Route::post('/addToShelf','HomeController@addToShelf')->middleware('verified');;
+Route::get('/count', 'HomeController@getNotificationCount')->middleware('verified');;
+Route::get('/getNotification','HomeController@getNotifications')->middleware('verified');;
+Route::post('/rateBook', 'HomeController@rateBook')->middleware('verified');;
+Route::get('/getProfileDetails','UserController@getProfileDetails')->middleware('verified');;
+Route::post('/editProfile', 'UserController@editProfile')->middleware('verified');;
+Route::get('/deleteShelfBook/{id}', 'UserController@deleteShelfBook')->middleware('verified');;
+Route::get('/getUserList','HomeController@getUserList')->middleware('verified');;
+Route::get('/showProfile/{id}', 'HomeController@showProfile')->middleware('verified');;
 
 //-----------------------------------------------------------------------------
 Route::get('/test', function(){
 	$user = Auth::user();
 	$user->notifications()->delete();
 });
-Route::get('/pendingRequest','HomeController@pendingRequest');
+Route::get('/pendingRequest','HomeController@pendingRequest')->middleware('verified');;
 //---------------------------------------------------------------------
-Route::get('/sendFriendRequest/{id}','UserController@sendFriendRequest');
-Route::get('/friendList','UserController@friendList');
-Route::get('/getFriendList','UserController@getFriendList');
-Route::get('/removeFriendRecord/{id}', 'UserController@deleteFriendship');
-Route::get('/acceptRequest/{id}','UserController@acceptRequest');
+Route::get('/sendFriendRequest/{id}','UserController@sendFriendRequest')->middleware('verified');;
+Route::get('/friendList','UserController@friendList')->middleware('verified');;
+Route::get('/getFriendList','UserController@getFriendList')->middleware('verified');;
+Route::get('/removeFriendRecord/{id}', 'UserController@deleteFriendship')->middleware('verified');;
+Route::get('/acceptRequest/{id}','UserController@acceptRequest')->middleware('verified');;
