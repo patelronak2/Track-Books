ed5ab1e9b445211437dd25fca37bd7ee146cb44c
Ronak Patel <patelronak2@hotmail.com>
2019-11-25 18:40:07 -0500

    messaging

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -30,16 +30,16 @@ return [
 
     'connections' => [
 
-        'pusher' => [
-            'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
-            'options' => [
-                'cluster' => 'us2',
-                'useTLS' => true,
-            ],
-        ],
+          'pusher' => [
+		  'driver' => 'pusher',
+		  'key' => env('PUSHER_APP_KEY'),
+		  'secret' => env('PUSHER_APP_SECRET'),
+		  'app_id' => env('PUSHER_APP_ID'),
+		  'options' => [
+			  'cluster' => env('PUSHER_CLUSTER'),
+			  'encrypted' => true,
+		  ],
+	  ],
 
         'redis' => [
             'driver' => 'redis',
