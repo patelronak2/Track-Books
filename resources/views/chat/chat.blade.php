6a1ae76c4ac4f15113ce3529038702794486c816
Ronak Patel <patelronak2@hotmail.com>
2019-11-25 21:47:00 -0500

    hi

--- resources/views/chat/chat.blade.php
+++ resources/views/chat/chat.blade.php
@@ -3,10 +3,11 @@
 @section('content')
 <div class="container-fluid">
 	<div class="row">
-	<div class="col-md-6 offset-md-6">
+	<div class="col-md-5 offset-md-3">
+		<h2>Group Chat</h2>
 	<div class="card">
 		<div class="card-header"> 
-			<h4>Chat</h4>
+			<h4>{{ Auth::user()->name }}</h4>
 		</div>
 		<div class="card-body">
 			<ul class="chat">
@@ -28,7 +29,8 @@
 			<div class="input-group mb-3">
 			  <input type="text" class="form-control" placeholder="Type your message here!" >
 			  <div class="input-group-append">
-				<span class="input-group-text badge badge-primary" id="basic-addon2">Send</span>
+				<span class="input-group-text">
+					<button class="badge badge-light button-sd m-1 p-2">Send</button></span>
 			  </div>
 			</div>
 		</div>
