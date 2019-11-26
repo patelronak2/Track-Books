f9dbda037a14a6614d8b0ebc8feb0f7cd57ea346
Ronak Patel <patelronak2@hotmail.com>
2019-11-25 21:07:01 -0500

    hi

--- resources/views/chat/chat.blade.php
+++ resources/views/chat/chat.blade.php
@@ -2,9 +2,29 @@
 
 @section('content')
 	<h2>Show some content here!</h2>
-	<chat-messages :messages="messages"></chat-messages>
-	<chat-form
-		v-on:messagesent="addMessage"
-		:user="{{ Auth::user() }}"
-	></chat-form>
+	<ul class="chat">
+        <li class="left clearfix" v-for="message in messages">
+            <div class="chat-body clearfix">
+                <div class="header">
+                    <strong class="primary-font">
+                        {{ message.user.name }}
+                    </strong>
+                </div>
+                <p>
+                    {{ message.message }}
+                </p>
+            </div>
+        </li>
+    </ul>
+	
+	<div class="input-group">
+        <input id="btn-input" type="text" name="message" class="form-control input-sm" placeholder="Type your message here..." v-model="newMessage" @keyup.enter="sendMessage">
+
+        <span class="input-group-btn">
+            <button class="btn btn-primary btn-sm" id="btn-chat" @click="sendMessage">
+                Send
+            </button>
+        </span>
+    </div>
+
 @endsection('content')
