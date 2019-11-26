15f0d2067712af0c06da3b11a845369b75afe370
Ronak Patel <patelronak2@hotmail.com>
2019-11-25 20:57:53 -0500

    hi

--- resources/views/chat/chat.blade.php
+++ resources/views/chat/chat.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.default')
 
 @section('content')
+	<h2>Show some content here!</h2>
 	<chat-messages :messages="messages"></chat-messages>
 	<chat-form
 		v-on:messagesent="addMessage"
