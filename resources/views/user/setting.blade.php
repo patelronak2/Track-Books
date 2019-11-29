c068663014fc9eb7f60c9062d561a76c296d1340
Ronak Patel <patelronak2@hotmail.com>
2019-11-29 08:08:33 -0500

    Comments

--- resources/views/user/setting.blade.php
+++ resources/views/user/setting.blade.php
@@ -20,7 +20,7 @@
 			 <div class="form-group row">
 				<label for="name" class="col-sm-2 col-form-label font-weight-bold">Name</label>
 				<div class="col-sm-10">
-				  <input type="text" class="form-control" id="name" value="John Doe">
+				  <input type="text" class="form-control" id="name">
 				</div>
 			  </div>
 			  <div class="form-group row">
